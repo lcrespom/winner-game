dec2c7aa72012131f2a07d29bd5c82eb304fe192
Luis Crespo <lcrespom@gmail.com>
2025-06-18 16:56:46 +0200

    Updated console test

--- tst/main.ts
+++ tst/main.ts
@@ -1,37 +1,23 @@
-import { GameParams, GameState, startGame, stepGame } from '../src/game-engine/engine.ts'
-
-const HELP_TURNS = 90
+import { type GameParams, startGame, stepGame } from '../src/game-engine/engine.ts'
 
 const params: GameParams = {
   numPlayers: 100,
   initialCoins: 100,
   turnsPerSecond: 100_000,
-  helpTurns: 0,
+  helpTurns: 90,
 }
 
 function runSimulation() {
   const NumFmt = Intl.NumberFormat()
   let state = startGame(params)
   let players = 0
-  let helpct = HELP_TURNS
   do {
     state = stepGame(state)
-    helpct--
-    if (HELP_TURNS > 0 && helpct == 0) {
-      helpEverybody(state)
-      helpct = HELP_TURNS
-    }
     if (players != state.players.length || state.turn % 1_000_000 == 0) {
       players = state.players.length
       console.log(`Step: ${NumFmt.format(state.turn)} - players: ${state.players.length}`)
     }
   } while (state.players.length > 1)
-}
-
-function helpEverybody(state: GameState) {
-  for (const player of state.players) {
-    player.coins++
-  }
 }
 
 runSimulation()
