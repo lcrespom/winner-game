257f099736197583f4e451099339d80b9a8675b1
Luis Crespo <lcrespom@gmail.com>
2025-06-19 17:33:41 +0200

    Game contros show information

--- src/components/game-controls.tsx
+++ src/components/game-controls.tsx
@@ -10,6 +10,8 @@ interface GameControlsProps {
   onParamsChange: (name: string, value: number) => void
 }
 
+const NumFmt = Intl.NumberFormat()
+
 const GameControls: React.FC<GameControlsProps> = ({
   state,
   onStart,
@@ -36,23 +38,35 @@ const GameControls: React.FC<GameControlsProps> = ({
   }
 
   return (
-    <div className="round-border relative my-4 w-full p-2">
-      <button className="absolute left-2" onClick={startClicked}>
-        {started ? 'Restart' : 'Start'}
-      </button>
-      <button className="absolute left-1/2 -translate-x-1/2" onClick={pauseClicked}>
-        {started ? (running ? 'Pause' : 'Continue') : 'Pause'}
-      </button>
-      <div className="absolute right-2">
-        Speed (turns per second)
-        <input
-          type="number"
-          className="ml-2 w-30"
-          value={state.params.turnsPerSecond}
-          onChange={e => onParamsChange('turnsPerSecond', parseInt(e.target.value))}
-        />
+    <div className="round-border my-4 w-full p-2">
+      <div className="relative">
+        <button className="absolute left-2" onClick={startClicked}>
+          {started ? 'Restart' : 'Start'}
+        </button>
+        <button className="absolute left-1/2 -translate-x-1/2" onClick={pauseClicked}>
+          {started ? (running ? 'Pause' : 'Continue') : 'Pause'}
+        </button>
+        <div className="absolute right-2">
+          Speed (turns per second)
+          <input
+            type="number"
+            className="ml-2 w-30"
+            value={state.params.turnsPerSecond}
+            onChange={e => onParamsChange('turnsPerSecond', parseInt(e.target.value))}
+          />
+        </div>
+        <button className="invisible">Center</button> {/* ensures container grows */}
       </div>
-      <button className="invisible">Center</button> {/* ensures container grows */}
+      <div className="relative">
+        <div className="absolute left-2 mt-4">
+          <b>Turn</b>: {NumFmt.format(state.turn)}
+        </div>
+        {/* TODO: time */}
+        <div className="absolute right-2 mt-4">
+          <b>Players</b>: {NumFmt.format(state.players.length)}
+        </div>
+        <button className="invisible">Center</button> {/* ensures container grows */}
+      </div>
     </div>
   )
 }
@@ -62,13 +76,14 @@ const GameControls: React.FC<GameControlsProps> = ({
     - [x] Start/restart button
     - [x] Pause/continue button
     - [x] Turns per frame input
+  - Information
+    - [x] Turn counter
+    - [ ] Elapsed time
+    - [x] Player counter
   - Game parameters
     - [ ] Number of players input
     - [ ] Initial coins input
     - [ ] Help turns input
-  - Information
-    - [ ] Turn counter
-    - [ ] Player counter
 */
 
 export default GameControls
