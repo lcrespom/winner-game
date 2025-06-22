0e3295e2dd1a1bb965bb2f244f80ba1f0bc64420
Luis Crespo <lcrespom@gmail.com>
2025-06-22 08:06:15 +0200

    Improved Y axis coordinates

--- src/components/game-graph.tsx
+++ src/components/game-graph.tsx
@@ -5,22 +5,41 @@ interface CoinBarChartProps {
   state: GameState
 }
 
+let lastMax = 0
+let lastTurn = 0
+
+const numFmt = Intl.NumberFormat()
+
+function roundUp(num: number, step: number = 20) {
+  return Math.ceil(num / step) * step
+}
+
+function getMaxCoins(state: GameState) {
+  if (state.turn < lastTurn) lastMax = 0 // Game has restarted
+  lastTurn = state.turn
+  const totalCoins = state.players.reduce((coins, player) => coins + player.coins, 0)
+  let max = Math.max(...state.players.map(p => p.coins), 1) * 2
+  if (max > totalCoins) max = totalCoins
+  max = roundUp(max)
+  if (lastMax > max) max = lastMax
+  else lastMax = max
+  return max
+}
+
 const YAxisValues: React.FC<{ maxValue: number }> = ({ maxValue }) => {
   return (
     <div className="absolute top-1 left-1 h-[calc(100%-1rem)] text-sm text-gray-500">
-      <span className="absolute">{maxValue}</span>
-      <span className="absolute top-1/4">{(maxValue * 3) / 4}</span>
-      <span className="absolute top-1/2">{(maxValue * 1) / 2}</span>
-      <span className="absolute top-3/4">{(maxValue * 1) / 4}</span>
+      <span className="absolute">{numFmt.format(maxValue)}</span>
+      <span className="absolute top-1/4">{numFmt.format((maxValue * 3) / 4)}</span>
+      <span className="absolute top-1/2">{numFmt.format((maxValue * 1) / 2)}</span>
+      <span className="absolute top-3/4">{numFmt.format((maxValue * 1) / 4)}</span>
     </div>
   )
 }
 
 export const CoinBarChart: React.FC<CoinBarChartProps> = ({ state }) => {
   const { players } = state
-  const maxCoins = players.reduce((coins, player) => coins + player.coins, 0)
-  // const maxCoins = Math.max(...players.map(p => p.coins), 1)
-
+  const maxCoins = getMaxCoins(state)
   return (
     <div className="round-border relative my-4 box-border flex h-[430px] w-full items-end p-1">
       {players.map(player => {
